f117e409d1f1a1a5ea664b4b916129b45be149e5
Zadkiel Aharonian <hello@zadkiel.fr>
2018-10-28 14:49:28 +0100

    WIP

--- digital-ocean/container-linux/kubernetes/bootkube.tf
+++ digital-ocean/container-linux/kubernetes/bootkube.tf
@@ -2,13 +2,15 @@
 module "bootkube" {
   source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=f39f8294c465397e622c606174e6f412ee3ca0f8"
 
-  cluster_name          = "${var.cluster_name}"
-  api_servers           = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
-  etcd_servers          = "${digitalocean_record.etcds.*.fqdn}"
-  asset_dir             = "${var.asset_dir}"
-  networking            = "flannel"
-  network_mtu           = 1440
-  pod_cidr              = "${var.pod_cidr}"
-  service_cidr          = "${var.service_cidr}"
-  cluster_domain_suffix = "${var.cluster_domain_suffix}"
+  cluster_name                              = "${var.cluster_name}"
+  api_servers                               = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
+  etcd_servers                              = "${digitalocean_record.etcds.*.fqdn}"
+  asset_dir                                 = "${var.asset_dir}"
+  networking                                = "flannel"
+  network_mtu                               = 1440
+  pod_cidr                                  = "${var.pod_cidr}"
+  service_cidr                              = "${var.service_cidr}"
+  cluster_domain_suffix                     = "${var.cluster_domain_suffix}"
+  cloud_provider                            = ""
+  apiserver_kubelet_preferred_address_types = "InternalIP,ExternalIP,Hostname"
 }
